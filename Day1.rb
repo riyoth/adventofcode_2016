cf692f6af9f791bad20801c463cc09344b98b74d
Pier-Olivier Clement <poclement@riyoth.info>
2016-12-23 18:45:07 -0500

    Clean up Day 1

--- Day1.rb
+++ Day1.rb
@@ -1,49 +1,40 @@
-
 class Day1
   DIRECTIONS = [[1,0],[0,1],[-1,0],[0,-1]]
   TURNS = { 'R' => 1, 'L' => -1}
 
-  def part1(instructions_string)
-    instructions = instructions_string.split(/\s*,\s*/)
+  def part1(input)
+    instructions = input.split(/\s*,\s*/)
 
     position = [0,0]
     direction = 0
 
-    instructions.each do |i|
-      direction += TURNS[i[0]]
-      position[0] += DIRECTIONS[direction %4 ][0] * i[1..-1].to_i
-      position[1] += DIRECTIONS[direction %4 ][1] * i[1..-1].to_i
+    instructions.each do |instruction|
+      direction += TURNS[instruction[0]]
+      position[0] += DIRECTIONS[direction %4][0] * instruction[1..-1].to_i
+      position[1] += DIRECTIONS[direction %4][1] * instruction[1..-1].to_i
     end
     return position[0].abs + position[1].abs
   end
 
-  def part2(instructions_string)
-    instructions = instructions_string.split(/\s*,\s*/)
+  def part2(input)
+    instructions = input.split(/\s*,\s*/)
 
     position = [0,0]
     direction = 0
     visited =[]
-    hq = []
 
-    instructions.each do |i|
-      direction += TURNS[i[0]]
-      (1..i[1..-1].to_i).each do
-        position[0] += DIRECTIONS[direction %4 ][0]
-        position[1] += DIRECTIONS[direction %4 ][1]
+    instructions.each do |instruction|
+      direction += TURNS[instruction[0]]
+      (1..instruction[1..-1].to_i).each do
+        position[0] += DIRECTIONS[direction %4][0]
+        position[1] += DIRECTIONS[direction %4][1]
 
-        if visited.include? position.join(",") then
+        if visited.include? position.join(",")
           return position[0].abs + position[1].abs
         end
-        
-        visited.push(position.join(","))
+
+        visited << position.join(",")
       end
-
     end
-
   end
-
-  def move()
-
-  end
-
 end
